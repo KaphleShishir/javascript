30e09a4e5e6a5de86e6acaf3e312b25e61c93d63
KaphleShishir <kaphleshishir3@gmail.com>
2024-12-27 04:49:00 +0000

    commit recent changes

---
+++ 01_basics/dataconversion.js
@@ -0,0 +1,15 @@
+"use strict";
+console.log("shishir")
+let name = "shishir"
+let num = "shishir"
+let nameInNum = Number(num)
+
+let a = 4
+console.log(typeof a)
+let a1 = String(a)
+console.log(typeof(a1))
+/* NaN is Not a number and if you convert boolean value in number it gives either 0 or 1
+for the name of the name and the and the value of the var */
+let logginIsTrue = 0
+let SeeBOLMaybe = Boolean(logginIsTrue)
+console.log(SeeBOLMaybe)
